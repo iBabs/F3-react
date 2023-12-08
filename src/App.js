21330ba7edc7e9b3db6c9d2416fd888ae5fa3130
iBabs <ibrahimbabalola8@gmail.com>
2023-12-08 14:51:31 +0100

    dec 12 class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Baba from './Baba';
 import { info } from './data';
 
 
+
 function App() {
 //  const oneper = info.filter(i=> i.name==='Gabby')
+const [mood, setMood] = useState("Happy");
+
+const changeMood =()=>{
+
+  if(mood!=="Happy"){setMood('Happy')
+}else{
+  setMood('Angry')}
+}
+
   return (
     <div className="App">
       {info.map(i=><Baba {...i}/>)}
-
-    
+    <div>
+      <h2>{mood}</h2>
+      <button onClick={changeMood}>Change mood</button>
+      </div>
     </div>
   );
 }
--- src/Baba.jsx
+++ src/Baba.jsx
@@ -1,8 +1,10 @@
+import './Baba.css'
+
 
 const Baba = ({id, name, details, img}) => {
   return (
-    <div key={id}>
-      <img src={img} alt={name} width={'300px'} />
+    <div key={id} className="card">
+      <img src={img} alt={name} width={300} />
       <h2>{name}</h2>
       <p>{details}</p>
     </div>
