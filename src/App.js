d74eab0e79bf21f9943737adeff37ef3337432a5
iBabs <ibrahimbabalola8@gmail.com>
2023-12-15 13:52:26 +0100

    statefull

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Baba from './Baba';
 import { info } from './data';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-
-function App() {
-//  const oneper = info.filter(i=> i.name==='Gabby')
-const [mood, setMood] = useState("Happy");
-
-const changeMood =()=>{
-
-  if(mood!=="Happy"){setMood('Happy')
-}else{
-  setMood('Angry')}
+const light={
+  backgroundColor: 'black',
+  color: 'white'
+}
+const dark={
+  backgroundColor: 'white',
+  color: 'black'
 }
 
+function App() {
+
+
+  const [mode, setMode] = useState(light)
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('number changed')
+  },[count])
+  const increase = () => {
+    setCount(count + 1)
+  }
+  const decrease = () => {
+
+    setCount(count - 1)
+    if (count <= 0) {
+      toast.error('number lesser than zero')
+    }
+  }
+  const change=()=>{
+    mode!==light? setMode(light):setMode(dark)
+  }
+
+
   return (
-    <div className="App">
-      {info.map(i=><Baba {...i}/>)}
-    <div>
-      <h2>{mood}</h2>
-      <button onClick={changeMood}>Change mood</button>
+    <div className="App"
+    style={mode}
+    >
+      <button onClick={change}>theme</button>
+      <ToastContainer position='bottom-right' theme='dark' />
+      {info.map(i => <Baba {...i} />)}
+      <div>
+        <h2>{count}</h2>
+        <button onClick={decrease}>-</button>
+        <button onClick={increase}>+</button>
       </div>
     </div>
   );
---
+++ src/Joy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Joy() {
+  return (
+    <div>
+        Joy
+    <h2>Joy is a girl</h2>
+    </div>
+  )
+}
+
+export default Joy
